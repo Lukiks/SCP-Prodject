2dd53fbf4f7e3c7f4c205160f10350d46280123b
Lukiks <lukaszkrywult02@gmail.com>
2020-09-24 01:08:50 +0200

    First Prolog models, Standard Assets

---
+++ SCP/Assets/_Scripts/Prolog/PrologSC.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PrologSC : MonoBehaviour
+{
+
+    public bool walking = false;
+    public GameObject flashLight;
+    public GameObject panel;
+
+    public Animator anim;
+    public Animator anim1;
+
+    public float timer1;
+
+    public AudioSource pstryk;
+    public AudioClip pstykclip;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (walking && Input.GetKeyDown(KeyCode.Q))
+        {
+            flashLight.active = !flashLight.active;
+            pstryk.PlayOneShot(pstykclip);
+        }
+
+        if (!walking)
+        {
+            flashLight.active = false;
+        }
+
+        if (!walking)
+        {
+            GetComponent<FirstPersonController>().enabled = false;
+        }
+        if (walking)
+        {
+            GetComponent<FirstPersonController>().enabled = true;
+        }
+
+        timer1 -= 1f * Time.deltaTime;
+
+        if (timer1 <= 0f)
+        {
+            panel.active = false;
+            walking = true;
+            anim.SetBool("Anim", true);
+            anim1.SetBool("Anim", true);
+
+        }
+
+    }
+
+
+
+}
---
+++ SCP/Assets/_Scripts/Prolog/TpBarier.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TpBarier : MonoBehaviour
+{
+
+    public GameObject barier;
+    public GameObject ob1;
+
+    public float timer;
+
+    void Update()
+    {
+        barier.transform.position = ob1.transform.position;
+        timer -= 1f * Time.deltaTime;
+        if (timer <= 0)
+            Destroy(barier);
+    }
+}
