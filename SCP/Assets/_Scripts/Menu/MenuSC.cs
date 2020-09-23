18f44e69c88e52dcc6d22fad7fb5eec181418757
Lukiks <lukaszkrywult02@gmail.com>
2020-09-23 17:09:37 +0200

    two language menu

---
+++ SCP/Assets/_Scripts/Menu/MenuSC.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSC : MonoBehaviour
+{
+
+    public int languageMenu = 0;
+    public GameObject[] panels;
+
+    private void Update()
+    {
+        for (int i = 0; i < panels.Length; i++)
+        {
+            if (i == languageMenu)
+                panels[i].active = true;
+            else
+                panels[i].active = false;
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ConinueGame()
+    {
+
+    }
+
+    public void LanguageUpdate(int language)
+    {
+        languageMenu = language;
+    }
+
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
